e42f69e245c1d37944057f2c7c329a615d69431d
maxim3014G <84927482+maxim3014G@users.noreply.github.com>
2024-12-06 22:48:05 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
-
-
-$(function(){
-    
-    
-    
-    
-    
-  $(window).scroll(function(){
-    
-    var divOffset = $('#nav-item').offset();
-       
-    hasNotReachedIt = true;
-      
-    if(window.scrollY > divOffset.top && hasNotReachedIt){
-
-       $('#navv').fadeOut(300);        
-        hasNotReachedIt = false;     
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
     }
-      
-    if(window.scrollY < divOffset.top){
+  });
+}
 
-       $('#navv').fadeIn(300);
-        
-        console.log('you are above me');
-        hasNotReachedIt = false;
-        
-    }
 
- })
- 
- 
- 
- 
-})
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('p');
+let elements_1 = document.querySelectorAll('h1');
+let elements_2 = document.querySelectorAll('h2');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+for (let elm of elements_1) {
+  observer.observe(elm); 
+}
+for (let elm of elements_2) {
+  observer.observe(elm); 
+}
+
+
